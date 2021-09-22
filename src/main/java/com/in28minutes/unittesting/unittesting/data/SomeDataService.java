611e0b2648a5119efe2d36c5cb943ccf69edbc96
Marc Fearby <marc.fearby@service.nsw.gov.au>
2021-09-22 22:00:51 +1000

    Implement other tests in Step 4 using stub

--- src/main/java/com/in28minutes/unittesting/unittesting/data/SomeDataService.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/data/SomeDataService.java
@@ -2,4 +2,5 @@ package com.in28minutes.unittesting.unittesting.data;
 
 public interface SomeDataService {
     int[] retrieveAllData();
+    void setAllData(int[] data);
 }
--- src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessStubTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessStubTest.java
@@ -5,21 +5,55 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SomeDataServiceStub implements SomeDataService {
+
+    private int[] data = new int[] {};
+
     @Override
     public int[] retrieveAllData() {
-        return new int[] { 1, 2, 3 };
+        return data;
+    }
+
+    @Override
+    public void setAllData(int[] data) {
+        this.data = data;
     }
 }
 
 public class SomeBusinessStubTest {
 
     @Test
-    public void calculateSum_usingStubbedService() {
+    public void calculateSumUsingDataService_basic() {
+        SomeDataService stub = new SomeDataServiceStub();
+        stub.setAllData(new int[] { 1, 2, 3 });
+
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        business.setSomeDataService(stub);
+
+        int actualResult = business.calculateSumUsingDataService();
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateSumUsingDataService_empty() {
         SomeBusinessImpl business = new SomeBusinessImpl();
         business.setSomeDataService(new SomeDataServiceStub());
 
         int actualResult = business.calculateSumUsingDataService();
-        int expectedResult = 6;
+        int expectedResult = 0;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateSumUsingDataService_oneValue() {
+        SomeDataService stub = new SomeDataServiceStub();
+        stub.setAllData(new int[] { 7 });
+
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        business.setSomeDataService(stub);
+
+        int actualResult = business.calculateSumUsingDataService();
+        int expectedResult = 7;
         assertEquals(expectedResult, actualResult);
     }
 

934188e513ecb4c3d35cd689a0587189e853e5c8
Marc Fearby <marc.fearby@service.nsw.gov.au>
2021-09-23 14:16:04 +1000

    Use @InjectMocks and @Mock (Step 8)

--- src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessMockTest.java
@@ -3,19 +3,33 @@ package com.in28minutes.unittesting.unittesting.business;
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessMockTest {
 
-    SomeBusinessImpl business = new SomeBusinessImpl();
+    @InjectMocks
+    SomeBusinessImpl business;
+
+    @Mock
     SomeDataService mockDataService = mock(SomeDataService.class);
 
-    @BeforeEach
-    public void beforeEach() {
-        business.setSomeDataService(mockDataService);
-    }
+//    If you are using JUnit 5, in the next lecture use
+//    @ExtendWith(MockitoExtension.class)
+//    instead of @RunWith(MockitoJUnitRunner.class)
+
+    // No longer needed with @InjectMocks and @Mock
+//    @BeforeEach
+//    public void beforeEach() {
+//        business.setSomeDataService(mockDataService);
+//    }
 
     @Test
     public void calculateSumUsingDataService_basic() {

6dc76ab816e08e400ec17af36cc567a9f738d1a5
Marc Fearby <marc.fearby@service.nsw.gov.au>
2021-10-26 21:47:12 +1100

    Up to step 13

--- src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -1,12 +1,14 @@
 package com.in28minutes.unittesting.unittesting.business;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 public class ListMockTest {
 
@@ -31,4 +33,37 @@ public class ListMockTest {
         assertEquals("in28minutes", listMock.get(0));
         assertNull(listMock.get(1));
     }
+
+    @Test
+    public void size_returnWithGenericParameters() {
+        when(listMock.get(anyInt())).thenReturn("in28minutes");
+        assertEquals("in28minutes", listMock.get(0));
+        assertEquals("in28minutes", listMock.get(1));
+    }
+
+    @Test
+    public void argumentCapturing() {
+        listMock.add("Some String");
+
+        // Verify
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(listMock).add(captor.capture());
+
+        assertEquals("Some String", captor.getValue());
+    }
+
+    @Test
+    public void multipleArgumentCapturing() {
+        listMock.add("Some String 1");
+        listMock.add("Some String 2");
+
+        // Verify
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(listMock, times(2)).add(captor.capture());
+
+        List<String> allValues = captor.getAllValues();
+        assertEquals("Some String 1", allValues.get(0));
+        assertEquals("Some String 2", allValues.get(1));
+    }
+
 }
